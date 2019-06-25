c03ec518cbc0467ad9a59a0596f8eb927ba9e65c
james darin <jdchandler88@gmail.com>
2019-06-24 23:45:15 -0400

    added tests for inverter logic unit

--- src/test/java/com/darchan/elements/impl/com/darchan/elements/impl/logic/InverterLogicTest.java
+++ src/test/java/com/darchan/elements/impl/com/darchan/elements/impl/logic/InverterLogicTest.java
@@ -1,9 +1,23 @@
 package com.darchan.elements.impl.com.darchan.elements.impl.logic;
 
+import com.darchan.elements.impl.Bus;
+import com.darchan.elements.impl.ConstantSignal;
+import com.darchan.elements.impl.logic.InverterLogicUnit;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InverterLogicTest {
     @Test
     public void shouldReturnTrueIfInputIsFalse() {
+        Bus bus = new Bus(ConstantSignal.OFF);
+        assertTrue(new InverterLogicUnit().evaluate(bus));
+    }
+
+    @Test
+    public void shouldReturnFalseIfInputIsTrue() {
+        Bus bus = new Bus(ConstantSignal.ON);
+        assertFalse(new InverterLogicUnit().evaluate(bus));
     }
 }
