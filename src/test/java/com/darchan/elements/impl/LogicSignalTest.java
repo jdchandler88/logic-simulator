a52d085ab2297b50d1f910fb312782687873e2c9
james darin <jdchandler88@gmail.com>
2019-06-28 19:19:36 -0400

    added tests for logic signal

--- src/test/java/com/darchan/elements/impl/LogicSignalTest.java
+++ src/test/java/com/darchan/elements/impl/LogicSignalTest.java
@@ -1,5 +1,7 @@
 package com.darchan.elements.impl;
 
+import com.darchan.elements.iface.IBus;
+import com.darchan.elements.iface.ILogicUnit;
 import com.darchan.elements.iface.ISignal;
 import com.darchan.elements.impl.logic.LogicalFalseLogicUnit;
 import com.darchan.elements.impl.logic.LogicalTrueLogicUnit;
@@ -38,6 +40,31 @@ class LogicSignalTest {
         assertAll(
                 () -> verify(sig1, times(1)).addSignalChangeListener(ArgumentMatchers.any()),
                 () -> verify(sig2, times(1)).addSignalChangeListener(ArgumentMatchers.any())
+        );
+    }
+
+    @Test
+    void shouldReflectSignalChangeWhenInputSignalChanges() {
+        Clock clock = new Clock(false);
+        Bus bus = new Bus(clock);
+        ILogicUnit logicUnit = new ILogicUnit() {
+            @Override
+            public boolean evaluate(IBus inputBus) {
+                //this is the clock. could have referenced it here for clarity,
+                //but this is how logic units should be written
+                return inputBus.getSignals().get(0).isOn();
+            }
+        };
+        LogicSignal cut = new LogicSignal(bus, logicUnit);
+        assertAll(
+                () -> assertFalse(cut.isOn()),
+                () -> {
+                    //this tests the logic signal's listening capability. when clock
+                    //changes, it will notify the logic signal and use the logic unit
+                    //above to set the logic signal's internal state
+                    clock.tick();   //make a signal change
+                    assertTrue(cut.isOn());
+                }
         );
     }
 
