729937a5c5a7bd7f038ce7344494171eb2502b16
james darin <jdchandler88@gmail.com>
2019-06-28 16:13:19 -0400

    abstract signal decorator did not implement signal change listener methods. add that implementation. this was exposed by writing a test`

--- src/main/java/com/darchan/elements/impl/AbstractSignalDecorator.java
+++ src/main/java/com/darchan/elements/impl/AbstractSignalDecorator.java
@@ -1,6 +1,7 @@
 package com.darchan.elements.impl;
 
 import com.darchan.elements.iface.ISignal;
+import com.darchan.elements.iface.ISignalChangeListener;
 
 public abstract class AbstractSignalDecorator implements ISignal {
 
@@ -14,5 +15,14 @@ public abstract class AbstractSignalDecorator implements ISignal {
 	public final boolean isOn() {
 		return this.wrapped.isOn();
 	}
-	
+
+	@Override
+	public void addSignalChangeListener(ISignalChangeListener listener) {
+		this.wrapped.addSignalChangeListener(listener);
+	}
+
+	@Override
+	public void removeSignalChangeListener(ISignalChangeListener listener) {
+		this.wrapped.removeSignalChangeListener(listener);
+	}
 }
---
+++ src/test/java/com/darchan/elements/impl/AbstractSignalDecoratorTest.java
@@ -0,0 +1,19 @@
+package com.darchan.elements.impl;
+
+import com.darchan.elements.iface.ISignalChangeListener;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractSignalDecoratorTest {
+
+    @Test
+    void isOnShouldReturnTrueIsWrappedIsTrue() {
+
+    }
+
+    @Test
+    void isOnShouldReturnFalseIfWrappedIsFalse() {
+
+    }
+}
