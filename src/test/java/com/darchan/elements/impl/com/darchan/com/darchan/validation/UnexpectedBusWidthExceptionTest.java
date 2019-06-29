203dc64ff381a3b7a27be496811341b2e6a2dfba
james darin <jdchandler88@gmail.com>
2019-06-28 21:05:06 -0400

    implemented exception for unexpected bus width

--- src/test/java/com/darchan/elements/impl/com/darchan/com/darchan/validation/UnexpectedBusWidthExceptionTest.java
+++ src/test/java/com/darchan/elements/impl/com/darchan/com/darchan/validation/UnexpectedBusWidthExceptionTest.java
@@ -1,29 +1,32 @@
 package com.darchan.elements.impl.com.darchan.com.darchan.validation;
 
+import com.darchan.com.darchan.validation.UnexpectedBusWidthException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UnexpectedBusWidthExceptionTest {
 
+    private UnexpectedBusWidthException cut = new UnexpectedBusWidthException(1, 2, 0);
+
     @Test
     void minimumShouldBeInput() {
-        fail();
+        assertEquals(1, cut.getMinimumWidth());
     }
 
     @Test
     void maximumShouldBeInput() {
-        fail();
+        assertEquals(2, cut.getMaximumWidth());
     }
 
     @Test
     void receivedShouldBeInput() {
-        fail();
+        assertEquals(0, cut.getActualWidth());
     }
 
     @Test
     void messageShouldBeExpectedValue() {
-        fail();
+        assertEquals("Expected bus width of [1, 2] but was 0.", cut.getMessage());
     }
 
 }
